c3aa3390145afd5009d7da499c31712c3f5d0ed4
Arstac <arnaucosta95@gmail.com>
2024-04-30 13:01:06 +0200

    carrito config

--- LaKultural/carrito/urls.py
+++ LaKultural/carrito/urls.py
@@ -5,4 +5,6 @@ app_name = 'carrito'
 
 urlpatterns = [
     path('', views.carrito, name='carrito'),
+    path('add/<int:producto_id>/', views.add_producto, name='add_producto'),
+    path('remove/<int:producto_id>/', views.remove_producto, name='remove_producto'),    
 ]
--- LaKultural/carrito/views.py
+++ LaKultural/carrito/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from tienda.models import Producto
+from .utils import get_carrito_or_create
 
 def carrito(request):
-    contexto = {'mensaje': "Hola, mundo. Este es mi contexto."}
+    carrito = get_carrito_or_create(request)
+    contexto = {'carrito': carrito}
     return render(request, 'carrito/carrito.html', contexto)
+
+def add_producto(request, producto_id):
+    carrito = get_carrito_or_create(request)
+    producto = get_object_or_404(Producto, id=producto_id)
+
+    carrito.productos.add(producto)
+    return render(request, 'carrito/carrito.html', {'carrito': carrito})
+
+def remove_producto(request, producto_id):
+    carrito = get_carrito_or_create(request)
+    producto = Producto.objects.get(id=producto_id)
+    
+    carrito.productos.remove(producto)
+    return render(request, 'carrito/carrito.html', {'carrito': carrito})
